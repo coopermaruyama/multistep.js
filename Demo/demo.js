a6e4d4f28dd8da0459b99369537fb6ec06c09094
coopermaruyama <cooperm@susteen.com>
2012-10-30 15:39:41 -0700

    bundled tipsy for tooltips

--- Demo/demo.js
+++ Demo/demo.js
@@ -3,6 +3,301 @@
   var $;
 
   (function($) {
+    var Tipsy, isElementInDOM, maybeCall;
+    maybeCall = function(thing, ctx) {
+      if (typeof thing === "function") {
+        return thing.call(ctx);
+      } else {
+        return thing;
+      }
+    };
+    isElementInDOM = function(ele) {
+      if ((function() {
+        var _results;
+        _results = [];
+        while (ele = ele.parentNode) {
+          _results.push(ele === document);
+        }
+        return _results;
+      })()) {
+        return true;
+      }
+      return false;
+    };
+    Tipsy = function(element, options) {
+      this.$element = $(element);
+      this.options = options;
+      this.enabled = true;
+      return this.fixTitle();
+    };
+    Tipsy.prototype = {
+      show: function() {
+        var $tip, actualHeight, actualWidth, gravity, pos, title, tp;
+        title = this.getTitle();
+        if (title && this.enabled) {
+          $tip = this.tip();
+          $tip.find(".tipsy-inner")[(this.options.html ? "html" : "text")](title);
+          $tip[0].className = "tipsy";
+          $tip.remove().css({
+            top: 0,
+            left: 0,
+            visibility: "hidden",
+            display: "block"
+          }).prependTo(document.body);
+          pos = $.extend({}, this.$element.offset(), {
+            width: this.$element[0].offsetWidth,
+            height: this.$element[0].offsetHeight
+          });
+          actualWidth = $tip[0].offsetWidth;
+          actualHeight = $tip[0].offsetHeight;
+          gravity = maybeCall(this.options.gravity, this.$element[0]);
+          tp = void 0;
+          switch (gravity.charAt(0)) {
+            case "n":
+              tp = {
+                top: pos.top + pos.height + this.options.offset,
+                left: pos.left + pos.width / 2 - actualWidth / 2
+              };
+              break;
+            case "s":
+              tp = {
+                top: pos.top - actualHeight - this.options.offset,
+                left: pos.left + pos.width / 2 - actualWidth / 2
+              };
+              break;
+            case "e":
+              tp = {
+                top: pos.top + pos.height / 2 - actualHeight / 2,
+                left: pos.left - actualWidth - this.options.offset
+              };
+              break;
+            case "w":
+              tp = {
+                top: pos.top + pos.height / 2 - actualHeight / 2,
+                left: pos.left + pos.width + this.options.offset
+              };
+          }
+          if (gravity.length === 2) {
+            if (gravity.charAt(1) === "w") {
+              tp.left = pos.left + pos.width / 2 - 15;
+            } else {
+              tp.left = pos.left + pos.width / 2 - actualWidth + 15;
+            }
+          }
+          $tip.css(tp).addClass("tipsy-" + gravity);
+          $tip.find(".tipsy-arrow")[0].className = "tipsy-arrow tipsy-arrow-" + gravity.charAt(0);
+          if (this.options.className) {
+            $tip.addClass(maybeCall(this.options.className, this.$element[0]));
+          }
+          if (this.options.fade) {
+            return $tip.stop().css({
+              opacity: 0,
+              display: "block",
+              visibility: "visible"
+            }).animate({
+              opacity: this.options.opacity
+            });
+          } else {
+            return $tip.css({
+              visibility: "visible",
+              opacity: this.options.opacity
+            });
+          }
+        }
+      },
+      hide: function() {
+        if (this.options.fade) {
+          return this.tip().stop().fadeOut(function() {
+            return $(this).remove();
+          });
+        } else {
+          return this.tip().remove();
+        }
+      },
+      fixTitle: function() {
+        var $e;
+        $e = this.$element;
+        if ($e.attr("title") || typeof ($e.attr("original-title")) !== "string") {
+          return $e.attr("original-title", $e.attr("title") || "").removeAttr("title");
+        }
+      },
+      getTitle: function() {
+        var $e, o, title;
+        title = void 0;
+        $e = this.$element;
+        o = this.options;
+        this.fixTitle();
+        title = void 0;
+        o = this.options;
+        if (typeof o.title === "string") {
+          title = $e.attr((o.title === "title" ? "original-title" : o.title));
+        } else {
+          if (typeof o.title === "function") {
+            title = o.title.call($e[0]);
+          }
+        }
+        title = ("" + title).replace(/(^\s*|\s*$)/, "");
+        return title || o.fallback;
+      },
+      tip: function() {
+        if (!this.$tip) {
+          this.$tip = $("<div class=\"tipsy\"></div>").html("<div class=\"tipsy-arrow\"></div><div class=\"tipsy-inner\"></div>");
+          this.$tip.data("tipsy-pointee", this.$element[0]);
+        }
+        return this.$tip;
+      },
+      validate: function() {
+        if (!this.$element[0].parentNode) {
+          this.hide();
+          this.$element = null;
+          return this.options = null;
+        }
+      },
+      enable: function() {
+        return this.enabled = true;
+      },
+      disable: function() {
+        return this.enabled = false;
+      },
+      toggleEnabled: function() {
+        return this.enabled = !this.enabled;
+      }
+    };
+    $.fn.tipsy = function(options) {
+      var binder, enter, eventIn, eventOut, get, leave, tipsy;
+      get = function(ele) {
+        var tipsy;
+        tipsy = $.data(ele, "tipsy");
+        if (!tipsy) {
+          tipsy = new Tipsy(ele, $.fn.tipsy.elementOptions(ele, options));
+          $.data(ele, "tipsy", tipsy);
+        }
+        return tipsy;
+      };
+      enter = function() {
+        var tipsy;
+        tipsy = get(this);
+        tipsy.hoverState = "in";
+        if (options.delayIn === 0) {
+          return tipsy.show();
+        } else {
+          tipsy.fixTitle();
+          return setTimeout((function() {
+            if (tipsy.hoverState === "in") {
+              return tipsy.show();
+            }
+          }), options.delayIn);
+        }
+      };
+      leave = function() {
+        var tipsy;
+        tipsy = get(this);
+        tipsy.hoverState = "out";
+        if (options.delayOut === 0) {
+          return tipsy.hide();
+        } else {
+          return setTimeout((function() {
+            if (tipsy.hoverState === "out") {
+              return tipsy.hide();
+            }
+          }), options.delayOut);
+        }
+      };
+      if (options === true) {
+        return this.data("tipsy");
+      } else if (typeof options === "string") {
+        tipsy = this.data("tipsy");
+        if (tipsy) {
+          tipsy[options]();
+        }
+        return this;
+      }
+      options = $.extend({}, $.fn.tipsy.defaults, options);
+      if (!options.live) {
+        this.each(function() {
+          return get(this);
+        });
+      }
+      if (options.trigger !== "manual") {
+        binder = (options.live ? "live" : "bind");
+        eventIn = (options.trigger === "hover" ? "mouseenter" : "focus");
+        eventOut = (options.trigger === "hover" ? "mouseleave" : "blur");
+        this[binder](eventIn, enter)[binder](eventOut, leave);
+      }
+      return this;
+    };
+    $.fn.tipsy.defaults = {
+      className: null,
+      delayIn: 0,
+      delayOut: 0,
+      fade: false,
+      fallback: "",
+      gravity: "n",
+      html: false,
+      live: false,
+      offset: 0,
+      opacity: 0.8,
+      title: "title",
+      trigger: "hover"
+    };
+    $.fn.tipsy.revalidate = function() {
+      return $(".tipsy").each(function() {
+        var pointee;
+        pointee = $.data(this, "tipsy-pointee");
+        if (!pointee || !isElementInDOM(pointee)) {
+          return $(this).remove();
+        }
+      });
+    };
+    $.fn.tipsy.elementOptions = function(ele, options) {
+      if ($.metadata) {
+        return $.extend({}, options, $(ele).metadata());
+      } else {
+        return options;
+      }
+    };
+    $.fn.tipsy.autoNS = function() {
+      if ($(this).offset().top > ($(document).scrollTop() + $(window).height() / 2)) {
+        return "s";
+      } else {
+        return "n";
+      }
+    };
+    $.fn.tipsy.autoWE = function() {
+      if ($(this).offset().left > ($(document).scrollLeft() + $(window).width() / 2)) {
+        return "e";
+      } else {
+        return "w";
+      }
+    };
+    return $.fn.tipsy.autoBounds = function(margin, prefer) {
+      return function() {
+        var $this, boundLeft, boundTop, dir;
+        dir = {
+          ns: prefer[0],
+          ew: (prefer.length > 1 ? prefer[1] : false)
+        };
+        boundTop = $(document).scrollTop() + margin;
+        boundLeft = $(document).scrollLeft() + margin;
+        $this = $(this);
+        if ($this.offset().top < boundTop) {
+          dir.ns = "n";
+        }
+        if ($this.offset().left < boundLeft) {
+          dir.ew = "w";
+        }
+        if ($(window).width() + $(document).scrollLeft() - $this.offset().left < margin) {
+          dir.ew = "e";
+        }
+        if ($(window).height() + $(document).scrollTop() - $this.offset().top < margin) {
+          dir.ns = "s";
+        }
+        return dir.ns + (dir.ew ? dir.ew : "");
+      };
+    };
+  })(jQuery);
+
+  (function($) {
     return $.fn.listenForChange = function(options) {
       var current_focus, jquery_object, settings;
       settings = $.extend({
@@ -148,7 +443,7 @@
         }
       };
       this.addClass('autovalid-form');
-      $('input,select', this).each(function() {
+      $('input[type=text],input[type=email]', this).each(function() {
         return $(this).listenForChange();
       });
       this.find('.step').first().css('display', 'block');
@@ -203,9 +498,7 @@
         var name;
         name = $(this).attr('name');
         if ($('input[name=' + name + ']').is(':checked')) {
-          return $('.error-text.radio').remove();
-        } else {
-          return $('input[name=' + name + ']').first().before('<p class="error-text radio">Select an option!</p>');
+          return $('input[name=' + name + ']').first().tipsy("hide");
         }
       });
       ($('input[type=checkbox]', form)).change(function(e) {
@@ -220,11 +513,12 @@
         if ($('input[name=' + inputName + ']').first().attr('max') != null) {
           max = $('input[name=' + inputName + ']').first().attr('max').match(/\d+/);
         }
-        if ($('input[name=' + inputName + ']').is(':checked') && $('input[name=checks]:checked').length <= max && $('input[name=checks]:checked').length >= min) {
-          return $('.error-text.checkbox').remove();
+        if ($('input[name=' + inputName + ']').is(':checked') && $('input[name=' + inputName + ']:checked').length <= max && $('input[name=' + inputName + ']:checked').length >= min) {
+          $('input[name=' + inputName + ']').first().tipsy("hide");
+          return $('input[name=' + inputName + ']').first().tipsy("hide");
         } else {
           $('.error-text.checkbox').remove();
-          return $('input[name=' + inputName + ']').first().before('<p class="error-text checkbox" style="top:' + ($('input[name=' + inputName + ']').first().position().top - 20) + 'px;">Select at least ' + min + ' boxes!</p>');
+          return $('input[name=' + inputName + ']').first().attr('original-title', 'Select at least ' + min + ' boxes!').tipsy("show");
         }
       });
       ($('select', form)).change(function() {
@@ -275,22 +569,25 @@
             return $(this).addClass('error');
           }
         });
-        $('input[type=radio]', thisStep).each(function() {
+        $('input[type=radio]:visible', thisStep).each(function() {
           var name;
           name = $(this).attr('name');
           window.totalitems++;
           if ($('input[name=' + name + ']').is(':checked') || $('input[name=' + name + ']:hidden').length || $('input[name=' + name + '][optional=yes]').length) {
-            window.validitems++;
+            return window.validitems++;
           } else {
-            $('.error-text.radio').remove();
-            $('input[name=' + name + ']').first().before('<p class="error-text radio">Select an option!</p>');
+            $('input[name=' + name + ']').first().attr('original-title', 'Select One!');
+            $('input[name=' + name + ']').first().tipsy({
+              trigger: 'manual',
+              gravity: 'e'
+            });
+            return $('input[name=' + name + ']').first().tipsy("show");
           }
-          return log(validitems + " " + totalitems);
         });
-        $('input[type=checkbox]', thisStep).each(function() {
+        $('input[type=checkbox]:visible', thisStep).each(function() {
           var inputName, max, min, name;
           window.totalitems++;
-          min = 1;
+          min = 0;
           max = 9999;
           inputName = $(this).prop('name').replace(/\[/, "\\[");
           inputName = inputName.replace(/\]/, "\\]");
@@ -301,14 +598,12 @@
             max = $('input[name=' + inputName + ']').first().attr('max').match(/\d+/);
           }
           name = $(this).attr('name');
-          if ($('input[name=' + inputName + ']').is(':checked') && $('input[name=checks]:checked').length <= max && $('input[name=checks]:checked').length >= min || $('input[name=' + inputName + ']').length) {
-            $('.error-text.checkbox').remove();
-            window.validitems++;
+          if ($('input[name=' + inputName + ']').is(':checked') && $('input[name=' + inputName + ']:checked').length <= max && $('input[name=' + inputName + ']:checked').length >= min) {
+            $('input[name=' + inputName + ']').first().tipsy("hide");
+            return window.validitems++;
           } else {
-            $('.error-text.checkbox').remove();
-            $('input[name=' + inputName + ']').first().before('<p class="error-text checkbox" style="top:' + ($('input[name=' + inputName + ']').first().position().top - 20) + 'px;">Select at least ' + min + ' boxes!</p>');
+            return $('input[name=' + inputName + ']').first().attr('original-title', 'Select at least ' + min + ' boxes!').tipsy("show");
           }
-          return console.log(validitems, totalitems);
         });
         if (window.totalitems === window.validitems) {
           if (thisStep.attr('id') !== "last-step") {
@@ -333,6 +628,23 @@
         return $('.progress').css('width', progress * 100 + "%");
       });
     }
+  });
+
+  $(function() {
+    $('input:not([type=button],[type=image],[type=submit],[type=radio],[type=checkbox]),select').each(function() {
+      return $(this).tipsy({
+        trigger: 'manual',
+        gravity: 'w',
+        fallback: 'fix this field!'
+      });
+    });
+    return $('input[type=checkbox]').each(function() {
+      return $(this).tipsy({
+        trigger: 'manual',
+        gravity: 'se',
+        fallback: 'Select one!'
+      });
+    });
   });
 
 }).call(this);
--- src/autovalid.coffee
+++ src/autovalid.coffee
@@ -1,3 +1,217 @@
+# tipsy, facebook style tooltips for jquery
+# version 1.0.0a
+# (c) 2008-2010 jason frame [jason@onehackoranother.com]
+# released under the MIT license
+(($) ->
+  maybeCall = (thing, ctx) ->
+    (if (typeof thing is "function") then (thing.call(ctx)) else thing)
+  isElementInDOM = (ele) ->
+    return true  if ele is document  while ele = ele.parentNode
+    false
+  Tipsy = (element, options) ->
+    @$element = $(element)
+    @options = options
+    @enabled = true
+    @fixTitle()
+  Tipsy:: =
+    show: ->
+      title = @getTitle()
+      if title and @enabled
+        $tip = @tip()
+        $tip.find(".tipsy-inner")[(if @options.html then "html" else "text")] title
+        $tip[0].className = "tipsy" # reset classname in case of dynamic gravity
+        $tip.remove().css(
+          top: 0
+          left: 0
+          visibility: "hidden"
+          display: "block"
+        ).prependTo document.body
+        pos = $.extend({}, @$element.offset(),
+          width: @$element[0].offsetWidth
+          height: @$element[0].offsetHeight
+        )
+        actualWidth = $tip[0].offsetWidth
+        actualHeight = $tip[0].offsetHeight
+        gravity = maybeCall(@options.gravity, @$element[0])
+        tp = undefined
+        switch gravity.charAt(0)
+          when "n"
+            tp =
+              top: pos.top + pos.height + @options.offset
+              left: pos.left + pos.width / 2 - actualWidth / 2
+          when "s"
+            tp =
+              top: pos.top - actualHeight - @options.offset
+              left: pos.left + pos.width / 2 - actualWidth / 2
+          when "e"
+            tp =
+              top: pos.top + pos.height / 2 - actualHeight / 2
+              left: pos.left - actualWidth - @options.offset
+          when "w"
+            tp =
+              top: pos.top + pos.height / 2 - actualHeight / 2
+              left: pos.left + pos.width + @options.offset
+        if gravity.length is 2
+          if gravity.charAt(1) is "w"
+            tp.left = pos.left + pos.width / 2 - 15
+          else
+            tp.left = pos.left + pos.width / 2 - actualWidth + 15
+        $tip.css(tp).addClass "tipsy-" + gravity
+        $tip.find(".tipsy-arrow")[0].className = "tipsy-arrow tipsy-arrow-" + gravity.charAt(0)
+        $tip.addClass maybeCall(@options.className, @$element[0])  if @options.className
+        if @options.fade
+          $tip.stop().css(
+            opacity: 0
+            display: "block"
+            visibility: "visible"
+          ).animate opacity: @options.opacity
+        else
+          $tip.css
+            visibility: "visible"
+            opacity: @options.opacity
+
+
+    hide: ->
+      if @options.fade
+        @tip().stop().fadeOut ->
+          $(this).remove()
+
+      else
+        @tip().remove()
+
+    fixTitle: ->
+      $e = @$element
+      $e.attr("original-title", $e.attr("title") or "").removeAttr "title"  if $e.attr("title") or typeof ($e.attr("original-title")) isnt "string"
+
+    getTitle: ->
+      title = undefined
+      $e = @$element
+      o = @options
+      @fixTitle()
+      title = undefined
+      o = @options
+      if typeof o.title is "string"
+        title = $e.attr((if o.title is "title" then "original-title" else o.title))
+      else title = o.title.call($e[0])  if typeof o.title is "function"
+      title = ("" + title).replace(/(^\s*|\s*$)/, "")
+      title or o.fallback
+
+    tip: ->
+      unless @$tip
+        @$tip = $("<div class=\"tipsy\"></div>").html("<div class=\"tipsy-arrow\"></div><div class=\"tipsy-inner\"></div>")
+        @$tip.data "tipsy-pointee", @$element[0]
+      @$tip
+
+    validate: ->
+      unless @$element[0].parentNode
+        @hide()
+        @$element = null
+        @options = null
+
+    enable: ->
+      @enabled = true
+
+    disable: ->
+      @enabled = false
+
+    toggleEnabled: ->
+      @enabled = not @enabled
+
+  $.fn.tipsy = (options) ->
+    get = (ele) ->
+      tipsy = $.data(ele, "tipsy")
+      unless tipsy
+        tipsy = new Tipsy(ele, $.fn.tipsy.elementOptions(ele, options))
+        $.data ele, "tipsy", tipsy
+      tipsy
+    enter = ->
+      tipsy = get(this)
+      tipsy.hoverState = "in"
+      if options.delayIn is 0
+        tipsy.show()
+      else
+        tipsy.fixTitle()
+        setTimeout (->
+          tipsy.show()  if tipsy.hoverState is "in"
+        ), options.delayIn
+    leave = ->
+      tipsy = get(this)
+      tipsy.hoverState = "out"
+      if options.delayOut is 0
+        tipsy.hide()
+      else
+        setTimeout (->
+          tipsy.hide()  if tipsy.hoverState is "out"
+        ), options.delayOut
+    if options is true
+      return @data("tipsy")
+    else if typeof options is "string"
+      tipsy = @data("tipsy")
+      tipsy[options]()  if tipsy
+      return this
+    options = $.extend({}, $.fn.tipsy.defaults, options)
+    unless options.live
+      @each ->
+        get this
+
+    unless options.trigger is "manual"
+      binder = (if options.live then "live" else "bind")
+      eventIn = (if options.trigger is "hover" then "mouseenter" else "focus")
+      eventOut = (if options.trigger is "hover" then "mouseleave" else "blur")
+      this[binder](eventIn, enter)[binder] eventOut, leave
+    this
+
+  $.fn.tipsy.defaults =
+    className: null
+    delayIn: 0
+    delayOut: 0
+    fade: false
+    fallback: ""
+    gravity: "n"
+    html: false
+    live: false
+    offset: 0
+    opacity: 0.8
+    title: "title"
+    trigger: "hover"
+
+  $.fn.tipsy.revalidate = ->
+    $(".tipsy").each ->
+      pointee = $.data(this, "tipsy-pointee")
+      $(this).remove()  if not pointee or not isElementInDOM(pointee)
+
+
+  
+  # Overwrite this method to provide options on a per-element basis.
+  # For example, you could store the gravity in a 'tipsy-gravity' attribute:
+  # return $.extend({}, options, {gravity: $(ele).attr('tipsy-gravity') || 'n' });
+  # (remember - do not modify 'options' in place!)
+  $.fn.tipsy.elementOptions = (ele, options) ->
+    (if $.metadata then $.extend({}, options, $(ele).metadata()) else options)
+
+  $.fn.tipsy.autoNS = ->
+    (if $(this).offset().top > ($(document).scrollTop() + $(window).height() / 2) then "s" else "n")
+
+  $.fn.tipsy.autoWE = ->
+    (if $(this).offset().left > ($(document).scrollLeft() + $(window).width() / 2) then "e" else "w")
+
+  
+
+  $.fn.tipsy.autoBounds = (margin, prefer) ->
+    ->
+      dir =
+        ns: prefer[0]
+        ew: ((if prefer.length > 1 then prefer[1] else false))
+
+      boundTop = $(document).scrollTop() + margin
+      boundLeft = $(document).scrollLeft() + margin
+      $this = $(this)
+      dir.ns = "n"  if $this.offset().top < boundTop
+      dir.ew = "w"  if $this.offset().left < boundLeft
+      dir.ew = "e"  if $(window).width() + $(document).scrollLeft() - $this.offset().left < margin
+      dir.ns = "s"  if $(window).height() + $(document).scrollTop() - $this.offset().top < margin
+      dir.ns + ((if dir.ew then dir.ew else ""))
+) jQuery
 (($) ->
   $.fn.listenForChange = (options) ->
     settings = $.extend(
@@ -95,7 +309,7 @@ $.fn.extend
 		
 		#init
 		this.addClass('autovalid-form')
-		$('input,select',this).each ->
+		$('input[type=text],input[type=email]',this).each ->
 			$(this).listenForChange()
 		this.find('.step').first().css('display','block')
 		this.find('.step:not(:first)').append('<a href="#" class="back"></a>');
@@ -121,7 +335,8 @@ $.fn.extend
 			# if $(this).val()? then $(this).addClass('success') else $(this).addClass('error')
 		($ 'input[type=radio]', form).change ->
 			name = $(this).attr('name')
-			if $('input[name='+name+']').is(':checked') then $('.error-text.radio').remove() else $('input[name='+name+']').first().before('<p class="error-text radio">Select an option!</p>')
+			if $('input[name='+name+']').is(':checked')
+        $('input[name='+name+']').first().tipsy("hide")
 		($ 'input[type=checkbox]', form).change (e) ->
 			min = 1
 			max = 9999
@@ -130,10 +345,11 @@ $.fn.extend
 			min = $('input[name='+inputName+']').first().attr('min').match(/\d+/) if $('input[name='+inputName+']').first().attr('min')?
 			max = $('input[name='+inputName+']').first().attr('max').match(/\d+/) if $('input[name='+inputName+']').first().attr('max')?
 			if $('input[name='+inputName+']').is(':checked') and $('input[name='+inputName+']:checked').length <= max and $('input[name='+inputName+']:checked').length >= min
-				$('.error-text.checkbox').remove()
+        $('input[name='+inputName+']').first().tipsy("hide")
+        $('input[name='+inputName+']').first().tipsy("hide")
 			else
 				$('.error-text.checkbox').remove()
-				$('input[name='+inputName+']').first().before('<p class="error-text checkbox" style="top:'+ ($('input[name='+inputName+']').first().position().top - 20) + 'px;">Select at least ' + min + ' boxes!</p>')
+				$('input[name='+inputName+']').first().attr('original-title','Select at least ' + min + ' boxes!').tipsy("show")
 		($ 'select', form).change ->
 			if $(this).val()? and ($ this).val() isnt '' then $(this).removeClass('error') else $(this).addClass('error')
 		($ 'textarea', form).change ->
@@ -170,32 +386,30 @@ $.fn.extend
 				else
 					$(this).addClass('error')
 			# validate radio
-			$('input[type=radio]', thisStep).each ->
+			$('input[type=radio]:visible', thisStep).each ->
 				name = $(this).attr('name')
 				window.totalitems++
 				if $('input[name='+name+']').is(':checked') or $('input[name='+name+']:hidden').length or $('input[name='+name+'][optional=yes]').length
 					window.validitems++ 
 				else
-					$('.error-text.radio').remove()
-					$('input[name='+name+']').first().before('<p class="error-text radio">Select an option!</p>')
-				log validitems + " " + totalitems
+					$('input[name='+name+']').first().attr('original-title','Select One!')
+					$('input[name='+name+']').first().tipsy({trigger:'manual',gravity:'e'})
+					$('input[name='+name+']').first().tipsy("show")
 			# validate checkboxes
-			$('input[type=checkbox]', thisStep).each ->
+			$('input[type=checkbox]:visible', thisStep).each ->
 				window.totalitems++
-				min = 1
+				min = 0
 				max = 9999
 				inputName = $(this).prop('name').replace /\[/, "\\["
 				inputName = inputName.replace /\]/, "\\]"
 				min = $('input[name='+inputName+']').first().attr('min').match(/\d+/) if $('input[name='+inputName+']').first().attr('min')?
 				max = $('input[name='+inputName+']').first().attr('max').match(/\d+/) if $('input[name='+inputName+']').first().attr('max')?
 				name = $(this).attr('name')
-				if $('input[name='+inputName+']').is(':checked') and $('input[name='+inputName+']:checked').length <= max and $('input[name='+inputName+']:checked').length >= min or $('input[name='+inputName+']').length #or if any are hidden
-					$('.error-text.checkbox').remove()
+				if $('input[name='+inputName+']').is(':checked') and $('input[name='+inputName+']:checked').length <= max and $('input[name='+inputName+']:checked').length >= min
+					$('input[name='+inputName+']').first().tipsy("hide")
 					window.validitems++
 				else
-					$('.error-text.checkbox').remove()		
-					$('input[name='+inputName+']').first().before('<p class="error-text checkbox" style="top:'+ ($('input[name='+inputName+']').first().position().top - 20) + 'px;">Select at least ' + min + ' boxes!</p>')
-				console.log validitems, totalitems
+					$('input[name='+inputName+']').first().attr('original-title','Select at least ' + min + ' boxes!').tipsy("show")
 			if window.totalitems is window.validitems
 				unless thisStep.attr('id') is "last-step"
 					thisStep.slideUp()
@@ -215,8 +429,11 @@ $.fn.extend
 			currentStep--
 			progress = currentStep/($ '.step', form).size()
 			$('.progress').css('width', progress*100+"%")
-
-
+$ ->
+	$('input:not([type=button],[type=image],[type=submit],[type=radio],[type=checkbox]),select').each ->
+		$(this).tipsy({trigger: 'manual', gravity: 'w', fallback: 'fix this field!'})
+	$('input[type=checkbox]').each ->
+		$(this).tipsy({trigger: 'manual', gravity: 'se', fallback: 'Select one!'})
 
 
 
